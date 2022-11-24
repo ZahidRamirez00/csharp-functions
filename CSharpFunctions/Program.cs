87c5aeae9162d160bcc5cb70a665aac659074bef
ZahidRamirez00 <zahidramirez.00@gmail.com>
2022-11-24 15:21:14 +0100

    Aggiungere i file di progetto.

---
+++ CSharpFunctions/Program.cs
@@ -0,0 +1,41 @@
+﻿//----------------- MAIN --------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//-.-.-.-.-.-.-.-.-.-.-.-.-.-.- FUNCTION -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
+
+void StampaArrayInteri(int[] array)
+{
+    string stampaArray = "[";
+
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(i == array.Length - 1)
+        {
+            stampaArray += array[i];
+        }
+        else
+        {
+            stampaArray+= array[i] + ", ";
+        }
+    }
+
+    stampaArray+= "]";
+
+    Console.WriteLine(stampaArray);
+}
